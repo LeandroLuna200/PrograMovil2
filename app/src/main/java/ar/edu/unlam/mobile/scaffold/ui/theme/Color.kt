e7464500fd6586f969f09ab4e2f384f6c5eda4ce
Leandro Luna <LeandroLuna200@users.noreply.github.com>
2023-11-14 00:52:49 -0300

    Update Color.kt

--- app/src/main/java/ar/edu/unlam/mobile/scaffold/ui/theme/Color.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffold/ui/theme/Color.kt
@@ -10,7 +10,7 @@ val Purple40 = Color(0xFF9C27B0)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-val CustomRed = Color(0xFFC24339) //FFC24339
-val CustomLightBlue= Color(0xFF067CDA)
+val CustomRed = Color(0xFFC24339) // FFC24339
+val CustomLightBlue = Color(0xFF067CDA)
 val CustomLightBlue2 = Color(0xFF7DC0F5)
 val Green = Color(0xFF7BEB7F)
